5d71ea9151162616f488bf60525f490912e9ab4f
Léo Gaspard <leo@gaspard.io>
2023-12-20 00:28:12 +0100

    implement example api

--- examples/basic/api/src/lib.rs
+++ examples/basic/api/src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+struct Authenticator;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct Foo;
+struct Bar;
+struct Baz;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+crdb::db! {
+    mod db auth Authenticator {
+        Foo,
+        Bar,
+        Baz,
     }
 }
