113721ba6844c7d9c59c9f6c5003585ab339038c
Léo Gaspard <leo@gaspard.io>
2023-12-20 00:05:06 +0100

    introduce a db! macro

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,30 @@ pub use api::{DbPtr, Object, User};
 
 #[cfg(feature = "client")]
 pub mod client;
+#[cfg(not(feature = "client"))]
+pub mod client {
+    #[macro_export]
+    macro_rules! generate_client {
+        ($($_:tt)*) => {}
+    }
+}
 
 #[cfg(feature = "server")]
 pub mod server;
+#[cfg(not(feature = "client"))]
+pub mod client {
+    #[macro_export]
+    macro_rules! generate_server {
+        ($($_:tt)*) => {}
+    }
+}
+
+#[macro_export]
+macro_rules! db {
+    ( $module:ident : $($object:ty),* $(,)* ) => {
+        mod $module {
+            $crate::client::generate_client!($($object),*);
+            $crate::server::generate_server!($($object),*);
+        }
+    }
+}
