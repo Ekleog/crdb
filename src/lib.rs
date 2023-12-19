5aa2fed09eea099a440b05ff573f269f76397188
Léo Gaspard <leo@gaspard.io>
2023-12-19 21:36:23 +0100

    scaffold server code

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[cfg(feature = "server")]
+mod server;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(feature = "server")]
+pub use server::Server;
---
+++ src/server.rs
@@ -0,0 +1,27 @@
+use anyhow::Context;
+use std::net::SocketAddr;
+
+pub struct Server {
+    _db: sqlx::PgPool,
+}
+
+impl Server {
+    pub async fn new(db_url: &str) -> anyhow::Result<Server> {
+        Ok(Server {
+            _db: sqlx::postgres::PgPoolOptions::new()
+                .max_connections(50)
+                .connect(&db_url)
+                .await
+                .with_context(|| format!("opening database {db_url:?}"))?,
+        })
+    }
+
+    pub async fn serve(self, addr: &SocketAddr) -> anyhow::Result<()> {
+        let listener = tokio::net::TcpListener::bind(addr)
+            .await
+            .with_context(|| format!("binding to {addr:?}"))?;
+        axum::serve(listener, axum::Router::new())
+            .await
+            .context("serving axum webserver")
+    }
+}
