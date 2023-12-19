b6be651a177ae7afaf2478f4cedfed6ad6f4a163
Léo Gaspard <leo@gaspard.io>
2023-12-19 22:41:29 +0100

    make DbPtr be parameterized on an actual type

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,6 @@
-use uuid::Uuid;
+use std::marker::PhantomData;
 
-pub struct DbPtr {
-    #[doc(hidden)]
-    pub id: Uuid,
-}
+use uuid::Uuid;
 
 pub struct User {
     pub id: Uuid,
@@ -16,4 +13,10 @@ pub trait Object: Default + for<'a> serde::Deserialize<'a> + serde::Serialize {
     fn can_apply(&self, user: &User, event: &Self::Event) -> anyhow::Result<bool>;
     fn apply(&mut self, event: &Self::Event, force_snapshot: impl Fn()) -> anyhow::Result<()>;
     fn is_heavy(&self) -> anyhow::Result<bool>;
+}
+
+pub struct DbPtr<T> {
+    #[doc(hidden)]
+    pub id: Uuid,
+    _phantom: PhantomData<T>,
 }
