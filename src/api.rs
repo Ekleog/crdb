69863bb1b3cf5202b724cbf8828a560c8f1ea352
Léo Gaspard <leo@gaspard.io>
2023-12-19 22:46:17 +0100

    flesh out apis

--- src/api.rs
+++ src/api.rs
@@ -10,13 +10,25 @@ pub trait Object: Default + for<'a> serde::Deserialize<'a> + serde::Serialize {
     type Event: for<'a> serde::Deserialize<'a> + serde::Serialize;
 
     fn uuid() -> &'static Uuid;
+    fn snapshot_version() -> u64;
+    fn from_old_snapshot(version: u64, data: serde_json::Value) -> Self;
+
     fn can_apply(&self, user: &User, event: &Self::Event) -> anyhow::Result<bool>;
     fn apply(&mut self, event: &Self::Event, force_snapshot: impl Fn()) -> anyhow::Result<()>;
     fn is_heavy(&self) -> anyhow::Result<bool>;
 }
 
-pub struct DbPtr<T> {
+pub struct DbPtr<T: Object> {
     #[doc(hidden)]
     pub id: Uuid,
     _phantom: PhantomData<T>,
+}
+
+impl<T: Object> DbPtr<T> {
+    pub fn from_uuid_unchecked(id: Uuid) -> DbPtr<T> {
+        DbPtr {
+            id,
+            _phantom: PhantomData,
+        }
+    }
 }
