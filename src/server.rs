84850b8dfc8f5c43f0681f264116519ca5a3b542
Léo Gaspard <leo@gaspard.io>
2023-12-19 23:00:58 +0100

    scaffold server code

--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,26 @@
 use crate::User;
 use anyhow::Context;
 use axum::http::StatusCode;
-use std::net::SocketAddr;
+use std::{marker::PhantomData, net::SocketAddr};
 
 pub trait Authenticator: for<'a> serde::Deserialize<'a> + serde::Serialize {
     fn authenticate(&self) -> Result<User, (StatusCode, String)>;
 }
 
-pub struct Server {
-    _db: sqlx::PgPool,
+pub trait Config {}
+
+macro_rules! generate {
+    () => {
+        // TODO: generate a Config impl and a (static) Db impl
+    };
 }
 
-impl Server {
+pub struct Server<C: Config> {
+    _db: sqlx::PgPool,
+    _phantom: PhantomData<C>,
+}
+
+impl<C: Config> Server<C> {
     pub async fn new(db_url: &str) -> anyhow::Result<Server> {
         Ok(Server {
             _db: sqlx::postgres::PgPoolOptions::new()
@@ -19,6 +28,7 @@ impl Server {
                 .connect(&db_url)
                 .await
                 .with_context(|| format!("opening database {db_url:?}"))?,
+            _phantom: PhantomData,
         })
     }
 
