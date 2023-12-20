ebdf0aea4bad190facefd963a92b266cbe731d74
Léo Gaspard <leo@gaspard.io>
2023-12-20 18:32:34 +0100

    document sealing of server::Config

--- src/server.rs
+++ src/server.rs
@@ -7,8 +7,18 @@ pub trait Authenticator: for<'a> serde::Deserialize<'a> + serde::Serialize {
     fn authenticate(&self) -> Result<User, (StatusCode, String)>;
 }
 
+// This module needs to actually be public, because `generate_server!` needs to be
+// able to implement Config. However, making it doc(hidden) makes it look as though
+// it is actually sealed in the documentation, which is good because we don't want
+// users to rely on any stability guarantees there.
+#[doc(hidden)]
+pub mod private {
+    #[doc(hidden)]
+    pub trait Sealed {}
+}
+
 /// Note: Implementation of this trait is supposed to be provided by `crdb::db!`
-pub trait Config {}
+pub trait Config: private::Sealed {}
 
 #[doc(hidden)]
 #[macro_export]
