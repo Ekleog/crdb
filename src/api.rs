abb4028e369f4b5245c468ce20603bf1e8bccae2
Léo Gaspard <leo@gaspard.io>
2023-12-24 19:35:34 +0100

    fix type

--- src/api.rs
+++ src/api.rs
@@ -27,8 +27,8 @@ pub enum Query {
     Not(Box<Query>),
 
     // Any/all the values in the array at JsonPathItem must match Query
-    AnyIn(Vec<JsonPathItem>, Query),
-    AllIn(Vec<JsonPathItem>, Query),
+    AnyIn(Vec<JsonPathItem>, Box<Query>),
+    AllIn(Vec<JsonPathItem>, Box<Query>),
 
     // JSON tests
     Eq(Vec<JsonPathItem>, serde_json::Value),
