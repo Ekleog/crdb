31658f9d68237ba26f10a79902f22e75d1432d68
Léo Gaspard <leo@gaspard.io>
2024-01-01 23:11:36 +0100

    improve a bit on event test

--- src/server/postgres_db/tests.rs
+++ src/server/postgres_db/tests.rs
@@ -35,4 +35,18 @@ async fn smoke_test(db: sqlx::PgPool) {
     db.submit::<TestObject1, _>(OBJECT_ID_1, EVENT_ID_2, Arc::new(TestEvent1::Clear), &db)
         .await
         .expect("clearing object 1 failed");
+    db.submit::<TestObject1, _>(OBJECT_ID_1, EVENT_ID_2, Arc::new(TestEvent1::Clear), &db)
+        .await
+        .expect("submitting duplicate event failed");
+    db.submit::<TestObject1, _>(
+        OBJECT_ID_1,
+        EVENT_ID_2,
+        Arc::new(TestEvent1::Set(b"foo".to_vec())),
+        &db,
+    )
+    .await
+    .expect_err("submitting duplicate event with different contents worked");
 }
+
+// TODO: fuzz the db (by having postgres create its test db in a tablespace in /tmp
+// to avoid burning through the disk)
