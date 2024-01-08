333fb1b4c52d6f764da9925ddcb66ead55ab27fc
Léo Gaspard <leo@gaspard.io>
2024-01-08 02:13:49 +0100

    split TestObjectPerms into separate file

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -5,90 +5,14 @@ use anyhow::Context;
 use std::any::Any;
 
 mod mem_db;
+mod object_perms;
 mod object_simple;
 mod stubs;
 
 pub use mem_db::MemDb;
+pub use object_perms::{TestEventPerms, TestObjectPerms};
 pub use object_simple::{TestEventSimple, TestObjectSimple};
 pub use stubs::*;
-
-#[derive(
-    Clone,
-    Debug,
-    Eq,
-    PartialEq,
-    bolero::generator::TypeGenerator,
-    deepsize::DeepSizeOf,
-    serde::Deserialize,
-    serde::Serialize,
-)]
-pub struct TestObjectPerms(pub User);
-
-#[derive(
-    Clone,
-    Debug,
-    Eq,
-    PartialEq,
-    bolero::generator::TypeGenerator,
-    deepsize::DeepSizeOf,
-    serde::Deserialize,
-    serde::Serialize,
-)]
-pub enum TestEventPerms {
-    Set(User),
-}
-
-impl Object for TestObjectPerms {
-    type Event = TestEventPerms;
-
-    fn type_ulid() -> &'static TypeId {
-        &TYPE_ID_2
-    }
-
-    async fn can_create<'a, C: CanDoCallbacks>(
-        &'a self,
-        _user: User,
-        _db: &'a C,
-    ) -> anyhow::Result<bool> {
-        unimplemented!()
-    }
-
-    async fn can_apply<'a, C: CanDoCallbacks>(
-        &'a self,
-        _user: User,
-        _event: &'a Self::Event,
-        _db: &'a C,
-    ) -> anyhow::Result<bool> {
-        unimplemented!()
-    }
-
-    async fn users_who_can_read<'a, C: CanDoCallbacks>(
-        &'a self,
-        _db: &'a C,
-    ) -> anyhow::Result<Vec<User>> {
-        Ok(vec![self.0])
-    }
-
-    fn apply(&mut self, event: &Self::Event) {
-        match event {
-            TestEventPerms::Set(u) => self.0 = *u,
-        }
-    }
-
-    fn is_heavy(&self) -> bool {
-        false
-    }
-
-    fn required_binaries(&self) -> Vec<BinPtr> {
-        Vec::new()
-    }
-}
-
-impl crate::Event for TestEventPerms {
-    fn required_binaries(&self) -> Vec<BinPtr> {
-        Vec::new()
-    }
-}
 
 #[derive(
     Clone,
---
+++ src/test_utils/object_perms.rs
@@ -0,0 +1,81 @@
+use super::ulid;
+use crate::{BinPtr, CanDoCallbacks, Object, TypeId, User};
+
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    bolero::generator::TypeGenerator,
+    deepsize::DeepSizeOf,
+    serde::Deserialize,
+    serde::Serialize,
+)]
+pub struct TestObjectPerms(pub User);
+
+#[derive(
+    Clone,
+    Debug,
+    Eq,
+    PartialEq,
+    bolero::generator::TypeGenerator,
+    deepsize::DeepSizeOf,
+    serde::Deserialize,
+    serde::Serialize,
+)]
+pub enum TestEventPerms {
+    Set(User),
+}
+
+impl Object for TestObjectPerms {
+    type Event = TestEventPerms;
+
+    fn type_ulid() -> &'static TypeId {
+        static TYPE: TypeId = TypeId(ulid("01HKKA237SXP3PWNE80DAX5PGH"));
+        &TYPE
+    }
+
+    async fn can_create<'a, C: CanDoCallbacks>(
+        &'a self,
+        _user: User,
+        _db: &'a C,
+    ) -> anyhow::Result<bool> {
+        unimplemented!()
+    }
+
+    async fn can_apply<'a, C: CanDoCallbacks>(
+        &'a self,
+        _user: User,
+        _event: &'a Self::Event,
+        _db: &'a C,
+    ) -> anyhow::Result<bool> {
+        unimplemented!()
+    }
+
+    async fn users_who_can_read<'a, C: CanDoCallbacks>(
+        &'a self,
+        _db: &'a C,
+    ) -> anyhow::Result<Vec<User>> {
+        Ok(vec![self.0])
+    }
+
+    fn apply(&mut self, event: &Self::Event) {
+        match event {
+            TestEventPerms::Set(u) => self.0 = *u,
+        }
+    }
+
+    fn is_heavy(&self) -> bool {
+        false
+    }
+
+    fn required_binaries(&self) -> Vec<BinPtr> {
+        Vec::new()
+    }
+}
+
+impl crate::Event for TestEventPerms {
+    fn required_binaries(&self) -> Vec<BinPtr> {
+        Vec::new()
+    }
+}
