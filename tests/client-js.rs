0dd4d08422dd7e77fd2c2ba262e7160f6231bd43
Léo Gaspard <leo@gaspard.io>
2024-01-09 05:08:19 +0100

    make ci able to run wasm32 tests

--- tests/client-js.rs
+++ tests/client-js.rs
@@ -3,7 +3,7 @@ use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
-fn pass() {
+async fn pass() {
     assert_eq!(1, 1);
 }
 
