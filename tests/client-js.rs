1df6b9deac706b3ed0157955b8ec56d56c964d25
Léo Gaspard <leo@gaspard.io>
2024-01-12 03:22:05 +0100

    make wasm tests look more like regular tests

--- tests/client-js.rs
+++ tests/client-js.rs
@@ -1,11 +1,11 @@
 #![cfg(all(feature = "client", target_arch = "wasm32"))]
 
 use crdb::crdb_internal::LocalDb;
-use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
+use wasm_bindgen_test::{wasm_bindgen_test as test, wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
-#[wasm_bindgen_test]
+#[test]
 async fn smoke_test() {
     let _db = LocalDb::connect("smoke-test").await.unwrap();
     //panic!("is_persistent: {:?}", db.is_persistent());
