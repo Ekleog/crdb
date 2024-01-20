13e4069c79b8a530ce27d4587e3e6310e7be7bc4
Léo Gaspard <leo@gaspard.io>
2024-01-20 21:56:29 +0100

    fix typo

--- src/server/postgres_db.rs
+++ src/server/postgres_db.rs
@@ -1539,7 +1539,7 @@ impl<Config: ServerConfig> Db for PostgresDb<Config> {
         Ok(())
     }
 
-    async fn remove(&self, object_id: ObjectId) -> crate::Result<bool> {
+    async fn remove(&self, object_id: ObjectId) -> crate::Result<()> {
         panic!("Tried removing {object_id:?} from server, but server is supposed to always keep all the history!")
     }
 
