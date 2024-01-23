0dd86d0b96a47d360b14f222a8e91c4051c5bfb7
Léo Gaspard <leo@gaspard.io>
2024-01-23 21:17:57 +0100

    increase timeouts to avoid intermittent ci failures

--- src/server/postgres_db/tests.rs
+++ src/server/postgres_db/tests.rs
@@ -6,8 +6,8 @@ mod fuzz_battle_royale;
 mod fuzz_sessions;
 mod fuzz_two_threads;
 
-const CHECK_NAMED_LOCKS_FOR: Duration = Duration::from_millis(500);
-const MAYBE_LOCK_TIMEOUT: Duration = Duration::from_millis(500);
+const CHECK_NAMED_LOCKS_FOR: Duration = Duration::from_millis(1000);
+const MAYBE_LOCK_TIMEOUT: Duration = Duration::from_millis(1000);
 
 #[sqlx::test]
 async fn smoke_test(db: sqlx::PgPool) {
