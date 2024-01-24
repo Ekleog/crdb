55afa7c41146a3e82385a48c0804b0e32312c209
Léo Gaspard <leo@gaspard.io>
2024-01-24 23:03:08 +0100

    define the plan for the Db trait

--- src/db_trait.rs
+++ src/db_trait.rs
@@ -5,7 +5,9 @@ use crate::{
 };
 use std::sync::Arc;
 
+// TODO(high): rename into CachedDb
 pub trait Db: 'static + CrdbSend + CrdbSync {
+    /// TODO(high): Returns the new latest snapshot if it actually changed
     fn create<T: Object, C: CanDoCallbacks>(
         &self,
         object_id: ObjectId,
@@ -14,6 +16,8 @@ pub trait Db: 'static + CrdbSend + CrdbSync {
         lock: bool,
         cb: &C,
     ) -> impl CrdbFuture<Output = crate::Result<()>>;
+
+    /// TODO(high): Returns the new latest snapshot if it actually changed
     fn submit<T: Object, C: CanDoCallbacks>(
         &self,
         object_id: ObjectId,
@@ -22,13 +26,14 @@ pub trait Db: 'static + CrdbSend + CrdbSync {
         cb: &C,
     ) -> impl CrdbFuture<Output = crate::Result<()>>;
 
+    // TODO(high): make into get_latest, only server needs actual get-full behavior
     fn get<T: Object>(
         &self,
         lock: bool,
         ptr: ObjectId,
     ) -> impl CrdbFuture<Output = crate::Result<FullObject>>;
-    /// Note: this function can also be used to populate the cache, as the cache will include
-    /// any item returned by this function.
+
+    // TODO(high): remove from Db trait? it has no impact on the Cache
     fn query<T: Object>(
         &self,
         user: User,
@@ -36,13 +41,17 @@ pub trait Db: 'static + CrdbSend + CrdbSync {
         q: &Query,
     ) -> impl CrdbFuture<Output = crate::Result<Vec<ObjectId>>>;
 
+    // TODO(high): remove from Db trait? it has no impact on the latest-snapshot cache anyway
     fn recreate<T: Object, C: CanDoCallbacks>(
         &self,
         time: Timestamp,
         object_id: ObjectId,
         cb: &C,
     ) -> impl CrdbFuture<Output = crate::Result<()>>;
+
+    // TODO(high): remove from the Db trait, it is only used by client and has no impact on the latest-snapshot cache anyway
     fn unlock(&self, object_id: ObjectId) -> impl CrdbFuture<Output = crate::Result<()>>;
+
     fn remove(&self, object_id: ObjectId) -> impl CrdbFuture<Output = crate::Result<()>>;
 
     fn create_binary(
@@ -50,6 +59,7 @@ pub trait Db: 'static + CrdbSend + CrdbSync {
         binary_id: BinPtr,
         data: Arc<[u8]>,
     ) -> impl CrdbFuture<Output = crate::Result<()>>;
+
     fn get_binary(
         &self,
         binary_id: BinPtr,
