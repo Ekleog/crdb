35802ce8187624a843197881703fb21ac52e16cc
Léo Gaspard <leo@gaspard.io>
2024-01-26 04:58:02 +0100

    switch fuzz_object_full to new fuzzer generator

--- src/test_utils/fuzz_object_full.rs
+++ src/test_utils/fuzz_object_full.rs
@@ -4,8 +4,9 @@ use super::fuzz_helpers::{
         self,
         crdb_internal::{Db, ResultExt},
         fts::SearchableString,
+        make_fuzzer_stuffs,
         test_utils::{self, *},
-        BinPtr, EventId, Object, ObjectId, Query, Timestamp, User,
+        BinPtr, EventId, ObjectId, Query, Timestamp, User,
     },
     make_db, make_fuzzer, run_query, run_vacuum, setup, Database, SetupState,
 };
@@ -13,166 +14,8 @@ use anyhow::Context;
 use std::sync::Arc;
 use ulid::Ulid;
 
-#[derive(Debug, arbitrary::Arbitrary, serde::Deserialize, serde::Serialize)]
-enum Op {
-    Create {
-        id: ObjectId,
-        created_at: EventId,
-        object: Arc<TestObjectFull>,
-    },
-    Submit {
-        object: usize,
-        event_id: EventId,
-        event: Arc<TestEventFull>,
-    },
-    Get {
-        object: usize,
-    },
-    Query {
-        user: User,
-        q: Query,
-    },
-    Recreate {
-        object: usize,
-        new_created_at: EventId,
-        data: Arc<TestObjectFull>,
-    },
-    Remove {
-        object: usize,
-    },
-    CreateBinary {
-        data: Arc<[u8]>,
-        fake_id: Option<BinPtr>,
-    },
-    Vacuum {
-        recreate_at: Option<Timestamp>,
-    },
-}
-
-struct FuzzState {
-    is_server: bool,
-    objects: Vec<ObjectId>,
-    mem_db: test_utils::MemDb,
-}
-
-impl FuzzState {
-    fn new(is_server: bool) -> FuzzState {
-        FuzzState {
-            is_server,
-            objects: Vec::new(),
-            mem_db: test_utils::MemDb::new(is_server),
-        }
-    }
-
-    fn object(&self, id: usize) -> ObjectId {
-        #[cfg(target_arch = "wasm32")]
-        let id = id % (self.objects.len() + 1); // make valid inputs more likely
-        self.objects.get(id).copied().unwrap_or_else(ObjectId::now)
-    }
-}
-
-async fn apply_op(db: &Database, s: &mut FuzzState, op: &Op) -> anyhow::Result<()> {
-    match op {
-        Op::Create {
-            id,
-            created_at,
-            object,
-        } => {
-            let mut object = object.clone();
-            Arc::make_mut(&mut object).standardize(*id);
-            if !object.can_create(USER_ID_NULL, *id, &s.mem_db).await? {
-                return Ok(());
-            }
-            s.objects.push(*id);
-            let pg = db.create(*id, *created_at, object.clone(), true, db).await;
-            let mem = s
-                .mem_db
-                .create(*id, *created_at, object.clone(), true, &s.mem_db)
-                .await;
-            cmp(pg, mem)?;
-        }
-        Op::Submit {
-            object,
-            event_id,
-            event,
-        } => {
-            let o = s.object(*object);
-            let pg = db
-                .submit::<TestObjectFull, _>(o, *event_id, event.clone(), db)
-                .await;
-            let mem = s
-                .mem_db
-                .submit::<TestObjectFull, _>(o, *event_id, event.clone(), &s.mem_db)
-                .await;
-            cmp(pg, mem)?;
-        }
-        Op::Get { object } => {
-            let o = s.object(*object);
-            let db = db
-                .get_latest::<TestObjectFull>(true, o)
-                .await
-                .wrap_context(&format!("getting {o:?} in database"));
-            let mem = s
-                .mem_db
-                .get_latest::<TestObjectFull>(true, o)
-                .await
-                .wrap_context(&format!("getting {o:?} in mem db"));
-            cmp(db, mem)?;
-        }
-        Op::Query { user, q } => {
-            run_query::<TestObjectFull>(&db, &s.mem_db, *user, None, q).await?;
-        }
-        Op::Recreate {
-            object,
-            new_created_at,
-            data,
-        } => {
-            if !s.is_server {
-                let o = s.object(*object);
-                let pg = db
-                    .recreate::<TestObjectFull, _>(o, *new_created_at, data.clone(), false, db)
-                    .await;
-                let mem = s
-                    .mem_db
-                    .recreate::<TestObjectFull, _>(
-                        o,
-                        *new_created_at,
-                        data.clone(),
-                        false,
-                        &s.mem_db,
-                    )
-                    .await;
-                cmp(pg, mem)?;
-            }
-        }
-        Op::Remove { object } => {
-            let _object = object; // TODO(test): implement for non-postgres databases
-        }
-        Op::CreateBinary { data, fake_id } => {
-            let id = fake_id.unwrap_or_else(|| crdb::hash_binary(&data));
-            let mem = s.mem_db.create_binary(id, data.clone()).await;
-            let pg = db.create_binary(id, data.clone()).await;
-            cmp(pg, mem)?;
-        }
-        Op::Vacuum { recreate_at } => {
-            run_vacuum(&db, &s.mem_db, *recreate_at).await?;
-        }
-    }
-    Ok(())
-}
-
-async fn fuzz_impl((cluster, is_server): &(SetupState, bool), ops: Arc<Vec<Op>>) -> Database {
-    let db = make_db(cluster).await;
-    let mut s = FuzzState::new(*is_server);
-    for (i, op) in ops.iter().enumerate() {
-        apply_op(&db, &mut s, op)
-            .await
-            .with_context(|| format!("applying {i}th op: {op:?}"))
-            .unwrap();
-        db.assert_invariants_generic().await;
-        db.assert_invariants_for::<TestObjectFull>().await;
-    }
-    db
+make_fuzzer_stuffs! {
+    (Full, TestObjectFull, TestEventFull),
 }
 
 make_fuzzer!("fuzz_object_full", fuzz, fuzz_impl);
@@ -197,8 +40,8 @@ async fn regression_postgres_and_indexeddb_considered_missing_binaries_the_other
     let cluster = setup();
     fuzz_impl(
         &cluster,
-        Arc::new(vec![Create {
-            id: ObjectId(Ulid::from_string("7R000000000000000000000026").unwrap()),
+        Arc::new(vec![CreateFull {
+            object_id: ObjectId(Ulid::from_string("7R000000000000000000000026").unwrap()),
             created_at: EventId(Ulid::from_string("00000000000000000000000000").unwrap()),
             object: Arc::new(TestObjectFull {
                 name: SearchableString::new(),
@@ -208,6 +51,7 @@ async fn regression_postgres_and_indexeddb_considered_missing_binaries_the_other
                 )],
                 users: vec![],
             }),
+            lock: true,
         }]),
     )
     .await;
@@ -220,8 +64,8 @@ async fn regression_postgres_crashed_on_null_byte_in_string() {
     fuzz_impl(
         &cluster,
         Arc::new(vec![
-            Create {
-                id: ObjectId(Ulid::from_string("00000000000000000000000000").unwrap()),
+            CreateFull {
+                object_id: ObjectId(Ulid::from_string("00000000000000000000000000").unwrap()),
                 created_at: EventId(Ulid::from_string("00000000000000000000000000").unwrap()),
                 object: Arc::new(TestObjectFull {
                     name: SearchableString::from("foo\0bar"),
@@ -229,9 +73,10 @@ async fn regression_postgres_crashed_on_null_byte_in_string() {
                     bins: vec![],
                     users: vec![],
                 }),
+                lock: true,
             },
-            Submit {
-                object: 0,
+            SubmitFull {
+                object_id: 0,
                 event_id: EventId(Ulid::from_string("00000000000000000000000000").unwrap()),
                 event: Arc::new(TestEventFull::Rename(String::from("bar\0foo"))),
             },
