feff83933dad00ad8519f11c28891fcb2583fb66
Léo Gaspard <leo@gaspard.io>
2024-02-17 07:00:30 +0100

    use constants

--- examples/basic/server/src/main.rs
+++ examples/basic/server/src/main.rs
@@ -1,5 +1,9 @@
 use anyhow::Context;
-use std::str::FromStr;
+use std::{str::FromStr, time::Duration};
+
+const CACHE_SIZE: usize = 32 * 1024 * 1024;
+const RECREATE_OLDER_THAN: Duration = Duration::from_secs(5 * 60);
+const KILL_SESSIONS_OLDER_THAN: Duration = Duration::from_secs(24 * 3600);
 
 // Make sure that the code works fine with multi-threading enabled
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
@@ -13,14 +17,14 @@ async fn main() -> anyhow::Result<()> {
     let _crdb_server = crdb::Server::new(
         basic_api::db::ServerConfig,
         db,
-        8 * 1024 * 1024,
+        CACHE_SIZE,
         // Vacuum every 2 minutes, recreating objects older than 5 minutes
         crdb::ServerVacuumSchedule::new(
             crdb::cron::Schedule::from_str("0 */2 * * * * *").unwrap(),
             crdb::chrono::Utc,
         )
-        .recreate_older_than(std::time::Duration::from_secs(5 * 60))
-        .kill_sessions_older_than(std::time::Duration::from_secs(24 * 3600)),
+        .recreate_older_than(RECREATE_OLDER_THAN)
+        .kill_sessions_older_than(KILL_SESSIONS_OLDER_THAN),
     )
     .await
     .context("creating crdb server")?;
