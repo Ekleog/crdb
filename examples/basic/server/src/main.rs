8f8c90897b927a7bb1008b923770d9fc9acdb38c
Léo Gaspard <leo@gaspard.io>
2024-02-17 06:38:38 +0100

    create the test server properly

--- examples/basic/server/src/main.rs
+++ examples/basic/server/src/main.rs
@@ -4,20 +4,22 @@ use std::str::FromStr;
 // Make sure that the code works fine with multi-threading enabled
 #[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() -> anyhow::Result<()> {
-    let db_url = "test";
+    let db_url = "basic-crdb";
     let db = crdb::sqlx::postgres::PgPoolOptions::new()
         .max_connections(50)
         .connect(&db_url)
         .await
         .with_context(|| format!("opening database {db_url:?}"))?;
-    crdb::Server::new(
+    let _crdb_server = crdb::Server::new(
         basic_api::db::ServerConfig,
         db,
         8 * 1024 * 1024,
+        // Vacuum every 2 minutes, recreating objects older than 5 minutes
         crdb::ServerVacuumSchedule::new(
-            crdb::cron::Schedule::from_str("").unwrap(),
+            crdb::cron::Schedule::from_str("0 2 * * * * *").unwrap(),
             crdb::chrono::Utc,
-        ),
+        )
+        .recreate_older_than(std::time::Duration::from_secs(5 * 60)),
     )
     .await
     .unwrap();
