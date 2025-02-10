77c3d7692c007ad7ff1fb251db853f074b9774fc
Léo Gaspard <leo@gaspard.io>
2025-02-11 00:40:58 +0100

    fix nix lsp warning

--- shell.nix
+++ shell.nix
@@ -3,34 +3,32 @@ let
 in
 pkgs.stdenv.mkDerivation {
   name = "crdb";
-  buildInputs = (
-    (with pkgs; [
-      cargo-bolero
-      cargo-hack
-      cargo-nextest
-      cargo-udeps
-      # chromedriver # TODO(misc-low): test for both gecko and chrome in ci and locally
-      # chromium
-      firefox
-      geckodriver
-      just
-      niv
-      samply
-      sqlx-cli
-      trunk
-      wasm-bindgen-cli_0_2_100
-      wasm-pack
-      watchexec
+  buildInputs = with pkgs; [
+    cargo-bolero
+    cargo-hack
+    cargo-nextest
+    cargo-udeps
+    # chromedriver # TODO(misc-low): test for both gecko and chrome in ci and locally
+    # chromium
+    firefox
+    geckodriver
+    just
+    niv
+    samply
+    sqlx-cli
+    trunk
+    wasm-bindgen-cli_0_2_100
+    wasm-pack
+    watchexec
 
-      (fenix.combine (with fenix; [
-        stable.cargo
-        stable.rustc
-        stable.clippy
-        stable.rust-src
-        rust-analyzer
-        targets.wasm32-unknown-unknown.stable.rust-std
-      ]))
-    ])
-  );
+    (fenix.combine (with fenix; [
+      stable.cargo
+      stable.rustc
+      stable.clippy
+      stable.rust-src
+      rust-analyzer
+      targets.wasm32-unknown-unknown.stable.rust-std
+    ]))
+  ];
   SQLX_OFFLINE="true"; # for rust-analyzer
 }
